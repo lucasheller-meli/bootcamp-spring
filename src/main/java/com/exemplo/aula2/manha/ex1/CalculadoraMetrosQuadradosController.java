a3744eeca5054d0b66f88b327bcdd0a15c2b8dc1
Marianne Bianca De Melo Bezerra <marbezerra@MacBook-Pro-de-meliadmin.local>
2021-05-27 16:55:38 -0300

    atividade casa

--- src/main/java/com/exemplo/aula2/manha/ex1/CalculadoraMetrosQuadradosController.java
+++ src/main/java/com/exemplo/aula2/manha/ex1/CalculadoraMetrosQuadradosController.java
@@ -1,4 +1,10 @@
 package com.exemplo.aula2.manha.ex1;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CalculadoraMetrosQuadradosController {
+
+
+
 }
---
+++ src/main/java/com/exemplo/aula2/manha/ex1/CalculadoraMetrosQuadradosService.java
@@ -0,0 +1,7 @@
+package com.exemplo.aula2.manha.ex1;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculadoraMetrosQuadradosService {
+}
