6899339785455ff167cfa72f8d9b20f7a2137b18
Marianne Bianca De Melo Bezerra <marbezerra@MacBook-Pro-de-meliadmin.local>
2021-05-27 11:19:20 -0300

    exemplo pessoa

--- src/main/java/com/exemplo/controller/NumeroRomanoController.java
+++ src/main/java/com/exemplo/controller/NumeroRomanoController.java
@@ -1,23 +1,38 @@
 package com.exemplo.controller;
 
+import com.exemplo.model.Pessoa;
 import com.exemplo.services.NumeroRomanoService;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class NumeroRomanoController {
 
     private final NumeroRomanoService numeroRomanoService;
+    private final Pessoa[] pessoas = new Pessoa[5];
+    private int index = 0;
 
     public NumeroRomanoController(NumeroRomanoService numeroRomanoService) {
         this.numeroRomanoService = numeroRomanoService;
     }
 
-    @GetMapping(value = "/romano/{numeroRomano}")
+    @GetMapping( "/romano/{numeroRomano}")
     public ResponseEntity<String> numeroRomano(@PathVariable final Integer numeroRomano){
         return ResponseEntity.ok(numeroRomanoService.converterRomanos(numeroRomano));
+    }
+
+    @PostMapping("/pessoa")
+    public ResponseEntity<Pessoa[]> adicionarPessoa(@RequestBody Pessoa pessoa){
+        if(index >= pessoas.length){
+            return ResponseEntity.badRequest().build();
+        }
+        pessoas[index] = pessoa;
+        index++;
+        return ResponseEntity.ok(pessoas);
     }
 
 }
---
+++ src/main/java/com/exemplo/model/Pessoa.java
@@ -0,0 +1,13 @@
+package com.exemplo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pessoa {
+    private String nome;
+    private Integer idade;
+}
